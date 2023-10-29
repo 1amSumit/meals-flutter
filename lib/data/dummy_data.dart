9b397240647cfd43c59795536c9bcf544e25fc5c
1amSumit <sj779619@gmail.com>
2023-10-29 18:35:59 +0530

    meal data added

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,4 +1,5 @@
 import "package:meals/modals/category.dart";
+import "package:meals/modals/meal.dart";
 import "package:flutter/material.dart";
 
 const availableCategories = [
@@ -12,4 +13,197 @@ const availableCategories = [
   Category(id: "c8", title: "Egyptian", color: Colors.lightBlueAccent),
   Category(id: "c9", title: "Roman", color: Colors.cyanAccent),
   Category(id: "c10", title: "Celtic", color: Colors.purple),
+];
+
+const dummyMeals = [
+  Meal(
+    id: 'm1',
+    categories: ['Italian', 'Pasta'],
+    title: 'Spaghetti with Tomato Sauce',
+    imgUrl: 'https://www.example.com/spaghetti.jpg',
+    ingredients: ['Spaghetti', 'Tomato Sauce', 'Oregano', 'Olive Oil'],
+    steps: [
+      'Cook spaghetti according to package instructions.',
+      'Heat olive oil, add tomato sauce, and season with oregano.',
+      'Serve over cooked spaghetti.'
+    ],
+    duration: 30,
+    complexity: Complexity.simple,
+    affordability: Affordability.pricey,
+    isGlutenFree: false,
+    isLactoseFree: true,
+    isVegan: true,
+    isVegetarian: true,
+  ),
+  Meal(
+    id: 'm2',
+    categories: ['Mexican', 'Fast Food'],
+    title: 'Tacos',
+    imgUrl: 'https://www.example.com/tacos.jpg',
+    ingredients: ['Tortillas', 'Ground Beef', 'Lettuce', 'Tomatoes', 'Cheese'],
+    steps: [
+      'Brown ground beef in a skillet.',
+      'Place tortillas on a flat surface and fill with beef, lettuce, tomatoes, and cheese.',
+      'Fold in half and serve hot.'
+    ],
+    duration: 20,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: false,
+    isLactoseFree: false,
+    isVegan: false,
+    isVegetarian: false,
+  ),
+  Meal(
+    id: 'm3',
+    categories: ['Chinese', 'Vegetarian'],
+    title: 'Vegetable Stir-Fry',
+    imgUrl: 'https://www.example.com/stirfry.jpg',
+    ingredients: ['Mixed Vegetables', 'Soy Sauce', 'Ginger', 'Garlic', 'Rice'],
+    steps: [
+      'Heat oil in a wok or skillet.',
+      'Add ginger and garlic, then stir-fry mixed vegetables.',
+      'Season with soy sauce and serve over cooked rice.'
+    ],
+    duration: 25,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: true,
+    isLactoseFree: true,
+    isVegan: true,
+    isVegetarian: true,
+  ),
+  Meal(
+    id: 'm4',
+    categories: ['American', 'Meat'],
+    title: 'BBQ Ribs',
+    imgUrl: 'https://www.example.com/ribs.jpg',
+    ingredients: ['Pork Ribs', 'BBQ Sauce', 'Salt', 'Pepper'],
+    steps: [
+      'Preheat the grill.',
+      'Season ribs with salt and pepper, then grill and baste with BBQ sauce until cooked through.',
+      'Serve hot.'
+    ],
+    duration: 60,
+    complexity: Complexity.simple,
+    affordability: Affordability.pricey,
+    isGlutenFree: true,
+    isLactoseFree: true,
+    isVegan: false,
+    isVegetarian: false,
+  ),
+  Meal(
+    id: 'm5',
+    categories: ['Mediterranean', 'Seafood'],
+    title: 'Grilled Salmon',
+    imgUrl: 'https://www.example.com/salmon.jpg',
+    ingredients: ['Salmon Fillet', 'Lemon', 'Olive Oil', 'Herbs'],
+    steps: [
+      'Preheat the grill.',
+      'Brush salmon with olive oil, lemon, and herbs, then grill until cooked.',
+      'Serve with a side of your choice.'
+    ],
+    duration: 25,
+    complexity: Complexity.simple,
+    affordability: Affordability.pricey,
+    isGlutenFree: true,
+    isLactoseFree: true,
+    isVegan: false,
+    isVegetarian: false,
+  ),
+  Meal(
+    id: 'm6',
+    categories: ['Indian', 'Vegetarian'],
+    title: 'Vegetable Curry',
+    imgUrl: 'https://www.example.com/curry.jpg',
+    ingredients: ['Mixed Vegetables', 'Curry Sauce', 'Rice'],
+    steps: ['Cook mixed vegetables in curry sauce.', 'Serve over cooked rice.'],
+    duration: 35,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: true,
+    isLactoseFree: true,
+    isVegan: true,
+    isVegetarian: true,
+  ),
+  Meal(
+    id: 'm7',
+    categories: ['Italian', 'Pizza'],
+    title: 'Margherita Pizza',
+    imgUrl: 'https://www.example.com/pizza.jpg',
+    ingredients: ['Pizza Dough', 'Tomato Sauce', 'Mozzarella Cheese', 'Basil'],
+    steps: [
+      'Roll out pizza dough, add tomato sauce, cheese, and basil.',
+      'Bake in a preheated oven until golden brown.',
+      'Slice and serve.'
+    ],
+    duration: 30,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: false,
+    isLactoseFree: false,
+    isVegan: false,
+    isVegetarian: true,
+  ),
+  Meal(
+    id: 'm8',
+    categories: ['Japanese', 'Seafood'],
+    title: 'Sushi Rolls',
+    imgUrl: 'https://www.example.com/sushi.jpg',
+    ingredients: ['Sushi Rice', 'Nori Seaweed', 'Fresh Fish', 'Vegetables'],
+    steps: [
+      'Lay out nori seaweed, add sushi rice, fish, and vegetables.',
+      'Roll up and cut into bite-sized pieces.',
+      'Serve with soy sauce and wasabi.'
+    ],
+    duration: 45,
+    complexity: Complexity.simple,
+    affordability: Affordability.pricey,
+    isGlutenFree: false,
+    isLactoseFree: true,
+    isVegan: false,
+    isVegetarian: false,
+  ),
+  Meal(
+    id: 'm9',
+    categories: ['Greek', 'Vegetarian'],
+    title: 'Greek Salad',
+    imgUrl: 'https://www.example.com/salad.jpg',
+    ingredients: [
+      'Cucumbers',
+      'Tomatoes',
+      'Olives',
+      'Feta Cheese',
+      'Olive Oil'
+    ],
+    steps: [
+      'Chop cucumbers, tomatoes, and olives, then crumble feta cheese on top.',
+      'Drizzle with olive oil and serve cold.'
+    ],
+    duration: 15,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: true,
+    isLactoseFree: false,
+    isVegan: false,
+    isVegetarian: true,
+  ),
+  Meal(
+    id: 'm10',
+    categories: ['Mexican', 'Vegetarian'],
+    title: 'Vegetarian Burrito',
+    imgUrl: 'https://www.example.com/burrito.jpg',
+    ingredients: ['Tortilla', 'Black Beans', 'Rice', 'Salsa', 'Guacamole'],
+    steps: [
+      'Fill tortilla with black beans, rice, salsa, and guacamole.',
+      'Fold and serve hot or as a burrito bowl.'
+    ],
+    duration: 20,
+    complexity: Complexity.simple,
+    affordability: Affordability.affordable,
+    isGlutenFree: false,
+    isLactoseFree: true,
+    isVegan: true,
+    isVegetarian: true,
+  ),
 ];
